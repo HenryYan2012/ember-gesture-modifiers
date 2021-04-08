db78921332b7d4a9aeda14df1fd1ea775c9a27a8
Henry Yan <henry.yanbf@gmail.com>
2021-04-08 12:08:32 +0800

    Replace 'this.useCapture' with 'this.capture'.

--- addon/modifiers/did-pan.js
+++ addon/modifiers/did-pan.js
@@ -26,30 +26,30 @@ export default class DidPanModifier extends Modifier {
       this.element.style.touchAction = 'none';
     }
 
-    this.element.addEventListener('pointerdown', this.didTouchStart, { capture: this.useCapture, passive: true });
-    this.element.addEventListener('pointermove', this.didTouchMove, { capture: this.useCapture, passive: !this.preventScroll });
-    this.element.addEventListener('pointerup', this.didTouchEnd, { capture: this.useCapture, passive: true });
-    this.element.addEventListener('pointercancel', this.didTouchEnd, { capture: this.useCapture, passive: true });
+    this.element.addEventListener('pointerdown', this.didTouchStart, { capture: this.capture, passive: true });
+    this.element.addEventListener('pointermove', this.didTouchMove, { capture: this.capture, passive: !this.preventScroll });
+    this.element.addEventListener('pointerup', this.didTouchEnd, { capture: this.capture, passive: true });
+    this.element.addEventListener('pointercancel', this.didTouchEnd, { capture: this.capture, passive: true });
 
     if (this.pointerTypes?.includes('mouse')) {
-      document.addEventListener('pointermove', this.documentPointerMove, { capture: this.useCapture, passive: !this.preventScroll });
-      document.addEventListener('pointercancel', this.documentPointerUp, { capture: this.useCapture, passive: true });
-      document.addEventListener('pointerup', this.documentPointerUp, { capture: this.useCapture, passive: true });
+      document.addEventListener('pointermove', this.documentPointerMove, { capture: this.capture, passive: !this.preventScroll });
+      document.addEventListener('pointercancel', this.documentPointerUp, { capture: this.capture, passive: true });
+      document.addEventListener('pointerup', this.documentPointerUp, { capture: this.capture, passive: true });
     }
   }
 
   removeEventListeners() {
     this.element.style.touchAction = null;
 
-    this.element.removeEventListener('pointerdown', this.didTouchStart, { capture: this.useCapture, passive: true });
-    this.element.removeEventListener('pointermove', this.didTouchMove, { capture: this.useCapture, passive: !this.preventScroll });
-    this.element.removeEventListener('pointerup', this.didTouchEnd, { capture: this.useCapture, passive: true });
-    this.element.removeEventListener('pointercancel', this.didTouchEnd, { capture: this.useCapture, passive: true });
+    this.element.removeEventListener('pointerdown', this.didTouchStart, { capture: this.capture, passive: true });
+    this.element.removeEventListener('pointermove', this.didTouchMove, { capture: this.capture, passive: !this.preventScroll });
+    this.element.removeEventListener('pointerup', this.didTouchEnd, { capture: this.capture, passive: true });
+    this.element.removeEventListener('pointercancel', this.didTouchEnd, { capture: this.capture, passive: true });
 
     if (this.pointerTypes?.includes('mouse')) {
-      document.removeEventListener('pointermove', this.documentPointerMove, { capture: this.useCapture, passive: !this.preventScroll });
-      document.removeEventListener('pointercancel', this.documentPointerUp, {capture: this.useCapture, passive: true});
-      document.removeEventListener('pointerup', this.documentPointerUp, {capture: this.useCapture, passive: true});
+      document.removeEventListener('pointermove', this.documentPointerMove, { capture: this.capture, passive: !this.preventScroll });
+      document.removeEventListener('pointercancel', this.documentPointerUp, {capture: this.capture, passive: true});
+      document.removeEventListener('pointerup', this.documentPointerUp, {capture: this.capture, passive: true});
     }
   }
 
